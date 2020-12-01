446025907ca8e15c1ca92fdc51a583ed9a614b11
jayraj138 <75326987+jayraj138@users.noreply.github.com>
2020-12-01 23:52:30 +0530

    Add files via upload
    "First Commit"

---
+++ src/components/Game.js
@@ -0,0 +1,89 @@
+import React, {Component} from 'react';
+import Board from './Board';
+
+export default class game extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            xIsNext:true,
+            stepnumber:0,
+            history:[
+                {squares:Array(9).fill(null)}
+            ]
+        }
+    }
+    jumpTo(step){
+        this.setState({
+            stepnumber:step,
+            xIsNext:(step%2)===0
+        })
+    }
+    handleClick(i){
+        const history = this.state.history.slice(0,this.state.stepnumber+1);
+        const current = history[history.length-1];
+        const squares = current.squares.slice();
+        const winner = calculatewinner(squares);
+        if(winner || squares[i]){
+            alert("End of Game");
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            history:history.concat({
+                squares:squares
+            }),
+            xIsNext:!this.state.xIsNext,
+            stepnumber:history.length
+        });
+    }
+    render(){
+        const history = this.state.history;
+        const current = history[this.state.stepnumber];
+        const winner = calculatewinner(current.squares);
+        const moves = history.map((step,move) => {
+            const desc = move ? 'Go to #' + move: 'Start game'; 
+            return(
+                <li key={move}>
+                    <button onClick={()=> {this.jumpTo(move)}}>
+                        {desc}
+                    </button>
+                </li>
+            )
+        });
+        let status;
+        if(winner){
+            alert("Winner is declared");
+            status = "Winner is" + winner;
+        }else{
+            status = "Next Player is " + (this.state.xIsNext ? 'X' : '0');
+        }
+
+        return(
+            <div className="game">
+                <div className="game-board">
+                    <Board onClick={(i) => this.handleClick(i)}
+                    squares = {current.squares} />
+                </div>
+                <div className="game-info">
+                    <div> {status} </div>
+                    <ul> {moves} </ul>
+                </div>
+            </div>
+        )
+    }
+}
+
+function calculatewinner(squares){
+    const lines = [
+        [0,1,2],[3,4,5],[6,7,8],
+        [0,3,6],[1,4,7],[2,5,8],
+        [0,4,8],[2,4,6]
+    ];
+    for(let i=0;i<lines.length;i++){
+        const [a,b,c] = lines[i];
+        if(squares[a] && squares[a]===squares[b] && squares[b]===squares[c]){
+            return squares[a];
+        }
+    }
+    return null;
+}
